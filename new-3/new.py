a600c9cc825cdc732d9550696d1bba1dc3ce29e5
kiranHublikar1999 <kiranhublikar420@gmail.com>
2025-10-16 16:48:28 +0530

    Add files via upload

---
+++ new-3/new.py
@@ -0,0 +1,13 @@
+# hai students
+# now i changed the author deatils in git
+print("hai")
+print("hello world")
+print("this is my first program")
+
+print("new line added")
+
+
+print("added new line 2")
+
+
+# new comment
